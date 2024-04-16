0eb73f4dc663aa6b225d9cf75e2c70b80691a555
Alaataher027 <alaataher027@gmail.com>
2024-04-16 09:34:37 +0200

    add task3

---
+++ task3_3.py
@@ -0,0 +1,21 @@
+################ 3 ##################
+import nltk
+from nltk.corpus import stopwords
+
+def get_stopwords(language):
+    try:
+        stop_words = stopwords.words(language)
+        print(f"Stopwords in {language}:")
+        print(stop_words)
+    except OSError:
+        print(f"Stopwords for {language} not available in NLTK. Please check if the language is supported.")
+
+def main():
+    languages = ["english", "spanish", "french", "german", "italian", "russian", "swedish", "arabic"]
+    for language in languages:
+        get_stopwords(language)
+        print()
+
+if __name__ == "__main__":
+    nltk.download('stopwords')
+    main()
